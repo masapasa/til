66f8a2cfad97055e2bb65f4ed998bd6baa4741cb
masapasa <apyakurel@gmail.com>
2022-11-22 13:43:57 +0100

    nbdev_export

--- til/_modidx.py
+++ til/_modidx.py
@@ -2,7 +2,7 @@
 
 d = { 'settings': { 'branch': 'master',
                 'doc_baseurl': '/til/',
-                'doc_host': 'https://masapasa.github.io',
-                'git_url': 'https://github.com/masapasa/til/',
+                'doc_host': 'https://apyakurel.github.io',
+                'git_url': 'https://github.com/apyakurel/til/',
                 'lib_path': 'til'},
   'syms': {}}
