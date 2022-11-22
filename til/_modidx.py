7cb9f6f115f4bbf8e40a9fda633a16cdf2e3b208
masapasa <apyakurel@gmail.com>
2022-11-22 13:41:01 +0100

    settings

--- til/_modidx.py
+++ til/_modidx.py
@@ -2,7 +2,7 @@
 
 d = { 'settings': { 'branch': 'master',
                 'doc_baseurl': '/til/',
-                'doc_host': 'https://muellerzr.github.io',
-                'git_url': 'https://github.com/muellerzr/til/',
+                'doc_host': 'https://masapasa.github.io',
+                'git_url': 'https://github.com/masapasa/til/',
                 'lib_path': 'til'},
   'syms': {}}
